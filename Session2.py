8c2dce7ac3f093512b94d1c86260fedd149e0225
Jack O'Brien <112610774+jadyobri@users.noreply.github.com>
2024-01-11 16:16:15 -0800

    Add files via upload

---
+++ Session2.py
@@ -0,0 +1,22 @@
+#floating point addition
+x = 3.96
+y = 0.24
+z = x+y
+print("3.96 + 0.24 =", z)
+print("The type is:",type(z))
+
+#integer difference
+x = 100
+y = 50
+z = x-y
+print("\n100 - 50 =", z)
+print("The type is:",type(z))
+
+#product of floating point number and integer
+x = 96
+y = 0.24
+z = 96*0.24
+print("\n96 * 0.24 =", z)
+print("The type is:",type(z))
+
+
